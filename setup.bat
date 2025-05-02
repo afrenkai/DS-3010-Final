51716faafe7f543d76d836e6c3572003d945c22c
Artem <frenkartemcool1@gmail.com>
2025-05-02 15:09:59 -0400

    cleaning up repo

--- setup.bat
+++
@@ -1,6 +0,0 @@
-echo "starting the process of getting data"
-python get_data.py
-python hierarchical_cv.py
-python get_sample.py
-
-echo "should have finished getting data, and printed a sample of the train split"
